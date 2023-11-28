88af4d4dddcb8a2d6f6759f28bca2195c2972678
Fady ElSayed ElHalfawy <fadyhalfawy@gmail.com>
2023-11-28 20:17:50 +0200

    Update the main page

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './css/App.css';
+import TodoWrapper from "./components/TodoWrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World!</p>
-      </header>
-    </div>
+    <TodoWrapper />
   );
 }
 
